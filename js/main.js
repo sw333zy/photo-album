f6891bcd58e08c4bf809fb92122478d8218986e5
J Ridgeway <sw333zy@gmail.com>
2016-11-02 13:19:54 -0400

    Latest work on photo album UI

--- js/main.js
+++ js/main.js
@@ -1,37 +1,33 @@
-(function(){
-'use strict';
+(function() {
+    'use strict';
 
-$('.photos').on('click', 'li', function zoomPic(){
-$(this).toggleClass('zoomed');
-$('main').toggleClass('zooming');
-$('nav').appendTo('.zoomed');
-
-
-});
-
-$('nav').on('click', '.next', function nextPic(event){
-  event.stopPropagation();
-  $('.zoomed').removeClass('zoomed').next().addClass('zoomed');
-  $('nav').appendTo('.zoomed');
-  console.log('.photos');
-  var i = $('.photos').length();
-  // console.log(i)
-  //   if (i 9){
-  //     $('.zoomed').removeClass('zoomed');
-  //   };
-
-});
-
-$('nav').on('click', '.previous', function lastPic(event){
-  event.stopPropagation();
-  $('.zoomed').removeClass('zoomed').prev().addClass('zoomed');
-  $('nav').appendTo('.zoomed');
-
-});
+    $('.photos').on('click', 'li', function zoomPic() {
+        $(this).toggleClass('zoomed');
+        $('main').toggleClass('zooming');
+        $('nav').appendTo('.zoomed');
 
 
+    });
 
+    $('nav').on('click', '.next', function nextPic(event) {
+        event.stopPropagation();
+        $('.zoomed').removeClass('zoomed').next().addClass('zoomed');
+        $('nav').appendTo('.zoomed');
+        console.log('.photos');
+        var i = $('.photos').length();
+        // console.log(i)
+        //   if (i 9){
+        //     $('.zoomed').removeClass('zoomed');
+        //   };
 
+    });
+
+    $('nav').on('click', '.previous', function lastPic(event) {
+        event.stopPropagation();
+        $('.zoomed').removeClass('zoomed').prev().addClass('zoomed');
+        $('nav').appendTo('.zoomed');
+
+    });
 
 
 
